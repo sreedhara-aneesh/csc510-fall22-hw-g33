e0754e169e90b60bdc434cf75c94299fba77bf9f
mkudith <mkudith@gmail.com>
2022-08-29 17:30:45 -0400

    commit: Author: Udith Kalburgi Date:  2022-08-29 17:30:33.158336
        Packaging tools added
    
        setup.py and requirements.txt added

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py and requirements.txt have a “Library” and “Application” distinction. 
+from setuptools import setup 
+
+setup(
+    name='CSC510-FALL22-HW-G33',
+    version='0.1.0',
+    author='G33',
+    author_email='ukalbur@ncsu.edu',
+    packages=['data', 'test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/CSC510-FALL22-HW-G33/',
+    license='LICENSE.txt',
+    description='HW 1',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
