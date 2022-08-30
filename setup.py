23d0ac46d3d82d8ec5d19aca9a2f3a9705b7063e
mkudith <mkudith@gmail.com>
2022-08-30 02:19:04 -0400

    update packages list dynamically

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 # setup.py and requirements.txt have a “Library” and “Application” distinction. 
 from setuptools import setup 
+from setuptools import find_packages
 
 setup(
     name='CSC510-FALL22-HW-G33',
     version='0.1.0',
     author='G33',
     author_email='ukalbur@ncsu.edu',
-    packages=['data', 'test'],
+    packages=find_packages(),
     scripts=[],
     url='http://pypi.python.org/pypi/CSC510-FALL22-HW-G33/',
     license='LICENSE.txt',
     description='HW 1',
     long_description=open('README.md').read(),
-    install_requires=[
-    ],
+    install_requires=[],
 )
