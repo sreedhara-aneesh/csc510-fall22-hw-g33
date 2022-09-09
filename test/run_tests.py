6b5223a56c271ce579b0849068041b73eccf399d
sreedhara-aneesh <sreedhara.aneesh@gmail.com>
2022-09-08 23:43:15 -0400

    reset wd of test engine after each test, update test readme

--- test/run_tests.py
+++ test/run_tests.py
@@ -7,9 +7,14 @@ import os
 failing_tests = []
 passing_tests = []
 
+file_pattern = re.compile(r"^test_.+[.]py$")
+method_pattern = re.compile(r"^test_.+|^test$")
+
+wd = os.getcwd()
+
 files = os.listdir(os.path.abspath(os.path.join(__file__, os.pardir)))
 for file in files:
-    if re.match("test_*", file):
+    if file_pattern.match(file):
         try:
             spec = importlib.util.spec_from_file_location(file[:-3], os.path.abspath(os.path.join(__file__, os.pardir, file)))
             foo = importlib.util.module_from_spec(spec)
@@ -17,7 +22,7 @@ for file in files:
             spec.loader.exec_module(foo)
             attrs = dir(foo)
             for attr in attrs:
-                if re.match("test_*", attr):
+                if method_pattern.match(attr):
                     print("-----")
                     print(f"{file}/{attr}")
                     try:
@@ -28,7 +33,8 @@ for file in files:
                         print("FAIL")
                         traceback.print_exc()
                         failing_tests.append(f"{file}/{attr}")
-                    print("-----")
+                    finally:
+                        print("-----")
         except Exception as e:
             print("-----")
             print(f"{file}/*")
@@ -36,13 +42,17 @@ for file in files:
             traceback.print_exc()
             print("-----")
             failing_tests.append(f"{file}/*")
+        finally:
+            # in case a test changed the WD, change it back
+            os.chdir(wd)
+        
 print("-----")
 print(f"PASSED: {len(passing_tests)}")
 print(f"FAILED: {len(failing_tests)}")
 print("-----")
-print(f"TESTS FAILING:")
-for failing_test in failing_tests:
-    print(f"- {failing_test}")
 if len(failing_tests) != 0:
+    print(f"TESTS FAILING:")
+    for failing_test in failing_tests:
+        print(f"- {failing_test}")
     exit(1)
 exit(0)
