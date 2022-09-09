7140d4c112993b974703e2f504218d7559694c60
sreedhara-aneesh <sreedhara.aneesh@gmail.com>
2022-09-08 22:11:51 -0400

    update test engine to deal with file errors

--- test/run_tests.py
+++ test/run_tests.py
@@ -10,24 +10,32 @@ passing_tests = []
 files = os.listdir(os.path.abspath(os.path.join(__file__, os.pardir)))
 for file in files:
     if re.match("test_*", file):
-        spec = importlib.util.spec_from_file_location(file[:-3], os.path.abspath(os.path.join(__file__, os.pardir, file)))
-        foo = importlib.util.module_from_spec(spec)
-        sys.modules[file[:-3]] = foo
-        spec.loader.exec_module(foo)
-        attrs = dir(foo)
-        for attr in attrs:
-            if re.match("test_*", attr):
-                print("-----")
-                print(f"{file}/{attr}")
-                try:
-                    getattr(foo, attr)()
-                    passing_tests.append(attr)
-                    print("PASS")
-                except Exception as e:
-                    print("FAIL")
-                    traceback.print_exc()
-                    failing_tests.append(f"{file}/{attr}")
-                print("-----")
+        try:
+            spec = importlib.util.spec_from_file_location(file[:-3], os.path.abspath(os.path.join(__file__, os.pardir, file)))
+            foo = importlib.util.module_from_spec(spec)
+            sys.modules[file[:-3]] = foo
+            spec.loader.exec_module(foo)
+            attrs = dir(foo)
+            for attr in attrs:
+                if re.match("test_*", attr):
+                    print("-----")
+                    print(f"{file}/{attr}")
+                    try:
+                        getattr(foo, attr)()
+                        passing_tests.append(attr)
+                        print("PASS")
+                    except Exception as e:
+                        print("FAIL")
+                        traceback.print_exc()
+                        failing_tests.append(f"{file}/{attr}")
+                    print("-----")
+        except Exception as e:
+            print("-----")
+            print(f"{file}/*")
+            print("FAIL - UNABLE TO WORK WITH FILE")
+            traceback.print_exc()
+            print("-----")
+            failing_tests.append(f"{file}/*")
 print("-----")
 print(f"PASSED: {len(passing_tests)}")
 print(f"FAILED: {len(failing_tests)}")
