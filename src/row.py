67512943ecbcabf79288f1c5ba9a4b518ef71016
rpmertz <rpmertz@ncsu.edu>
2022-09-14 14:46:32 -0400

    remove row and eg.stats

--- src/row.py
+++
@@ -1,8 +0,0 @@
-# row holds one record
-class Row:
-    def __init__(t):
-        return {
-            "cells": t,  # one record
-            "cooked": t.copy(),  # used if we discretize data
-            "isEvaled": False,  # true if y-values evaluated
-        }
