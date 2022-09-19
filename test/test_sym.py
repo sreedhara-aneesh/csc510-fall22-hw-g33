c7660c3c8616133ce60c17a13a24176fc1b037ba
mkudith <mkudith@gmail.com>
2022-09-19 10:01:13 -0400

    test sym fixed

--- test/test_sym.py
+++ test/test_sym.py
@@ -12,10 +12,9 @@ def test():
     mode, entropy = sym_obj.mid(-1, -1, -1), round (sym_obj.div(-1, -1), 3)
 
     the_obj = the ()
-    print (the_obj.oo ({'mid': mode, 'div': entropy}))
+    print ({'mid': mode, 'div': entropy})
 
     test_result = (mode=="a" and 1.37 <= entropy and entropy <=1.38)
-    
     assert test_result
 
 # test ()
