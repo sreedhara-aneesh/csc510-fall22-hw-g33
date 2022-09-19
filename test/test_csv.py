a587ef048802aaa60a7f16d9b8d997b25c2e9e8e
mkudith <mkudith@gmail.com>
2022-09-19 10:17:07 -0400

    test csv fixed to work with oo helper

--- test/test_csv.py
+++ test/test_csv.py
@@ -3,10 +3,8 @@ import os
 sys.path.append(os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, 'src')))
 from num import Num
 from row import Row # TODO use Row class from other commit 
-from the import the
 from utils import Utils
-
-
+import the as the_class
 
 def checkcsv(n):
     test_result = True
@@ -15,16 +13,14 @@ def checkcsv(n):
     if rows> 10:
         return
     else:
-        the.oo(n)
+        the_obj = the_class.the ()
+        the_obj.oo(n)
     assert test_result
-
-def test_csv():
-    csv_filename = "./data/auto93.csv"
-    Utils.csv(csv_filename, checkcsv)
 
 def test():
     global rows
     rows = 0
-    test_csv()
+    csv_filename = "./data/auto93.csv"
+    Utils.csv(csv_filename, checkcsv)
 
 # test()
